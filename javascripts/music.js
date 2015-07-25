7278ae8c74630a039f4aea1688cd588d5b5472f7
errovenus <netnut@yeah.net>
2015-07-25 11:10:38 +0800

    换用audio实现
    可以控制播放和暂停，火狐、ie，猎豹、google，百度浏览器均测试可以。

--- javascripts/music.js
+++ javascripts/music.js
@@ -2,16 +2,20 @@ var bPlay = true;
 
 function playControl()
 {
+	var ms = document.getElementById("music_status");
+	var bm = document.getElementById("bgMusic");
+
 	if (bPlay)
 	{
-		var ms = document.getElementById("music_status");
-		ms.innerHTML = "PAUSE <strong>MUSIC</strong>";
+		ms.innerHTML = "PLAY <strong>MUSIC</strong>";
+		bm.pause();	///< ÔÝÍ£
 		bPlay = false;
 	}
 	else
 	{
 		var ms = document.getElementById("music_status");
-		ms.innerHTML = "PLAY <strong>MUSIC</strong>";
+		ms.innerHTML = "PAUSE <strong>MUSIC</strong>";
+		bm.play(); ///< ²¥·Å
 		bPlay = true;
 	}
 }
