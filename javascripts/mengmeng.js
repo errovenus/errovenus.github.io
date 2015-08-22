100bb845bc1bca8e07bd183c2848f00cd51e85ac
HuangLei <huangleililei@gmail.com>
2015-08-22 12:35:28 +0800

    左右两边添加图片
    好玩。顺便试下oncrool事件，但是我的左右两边是嵌套的html，所以好像有问题。。。有空接着弄。。。

---
+++ javascripts/mengmeng.js
@@ -0,0 +1,11 @@
+function mengshow()
+{
+	var lm = document.getElementById("leftmeng");
+	var rm = document.getElementById("rightmeng");
+
+	//window.alert("xxx");
+	//lm.style.top = document.body.scrollTop + 10;
+	//lm.style.left = document.body.scrollLeft;
+	//rm.style.top = document.body.scrollTop + 10;
+	//rm.style.right = document.body.scrollRight;
+}
