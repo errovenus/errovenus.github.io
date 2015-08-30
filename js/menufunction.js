73c88535a6b9b884b73d5d855bd7a3df76b1f2ce
HuangLei <huangleililei@gmail.com>
2015-08-30 14:44:59 +0800

    替换模板
    比较喜欢这种风格

---
+++ js/menufunction.js
@@ -0,0 +1,11 @@
+function showTipTodo()
+{
+	window.alert("Å¬Á¦ÖÐ...");
+}
+
+function printPage()
+{
+	document.getElementById("functionHeader").style.visibility="hidden";
+	window.print();
+	document.getElementById("functionHeader").style.visibility="visible";
+}
---
+++ js/verify.js
@@ -0,0 +1,14 @@
+function toVaild()
+{
+	var cc = document.getElementById("cc");
+	var bcc = document.getElementById("bcc");
+	if(cc.value == "√‹ÀÕ")
+	{
+		cc.value="";
+	}
+	if(bcc.value == "≥≠ÀÕ")
+	{
+		bcc.value="";
+	}
+	return true;
+}
